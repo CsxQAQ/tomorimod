2c852125c0a3e4821e4b0c629b9ad81ec369e803
陈书玄 <1257150224@qq.com>
2024-12-04 23:52:26 +0800

    添加了一个新的遗物

--- BasicMod-master/src/main/java/tomorinmod/BasicMod.java
+++ BasicMod-master/src/main/java/tomorinmod/BasicMod.java
@@ -32,6 +32,7 @@ import java.util.*;
 
 @SpireInitializer
 public class BasicMod implements
+        EditRelicsSubscriber,
         EditCardsSubscriber,
         EditCharactersSubscriber,
         EditStringsSubscriber,
@@ -240,4 +241,20 @@ public class BasicMod implements
                 .cards(); //Adds the cards
     }
 
+    @Override
+    public void receiveEditRelics() { //somewhere in the class
+        new AutoAdd(modID) //Loads files from this mod
+                .packageFilter(BaseRelic.class) //In the same package as this class
+                .any(BaseRelic.class, (info, relic) -> { //Run this code for any classes that extend this class
+                    if (relic.pool != null)
+                        BaseMod.addRelicToCustomPool(relic, relic.pool); //Register a custom character specific relic
+                    else
+                        BaseMod.addRelic(relic, relic.relicType); //Register a shared or base game character specific relic
+
+                    //If the class is annotated with @AutoAdd.Seen, it will be marked as seen, making it visible in the relic library.
+                    //If you want all your relics to be visible by default, just remove this if statement.
+                    if (info.seen)
+                        UnlockTracker.markRelicAsSeen(relic.relicId);
+                });
+    }
 }
