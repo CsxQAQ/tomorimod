fdad07da8805f2bd2e3faf04e3ab10b80572497d
陈书玄 <1257150224@qq.com>
2024-12-12 13:51:11 +0800

    实现独角仙的升级+稀有度，之后还要实现代替任意素材的功能

---
+++ BasicMod-master/src/main/java/tomorinmod/cards/uncommon/RhinocerosBeetle.java
@@ -0,0 +1,48 @@
+package tomorinmod.cards.uncommon;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PlatedArmorPower;
+import tomorinmod.cards.BaseCard;
+import tomorinmod.character.MyCharacter;
+import tomorinmod.util.CardStats;
+
+public class RhinocerosBeetle extends BaseCard {
+    public static final String ID = makeID(RhinocerosBeetle.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF,
+            0
+    );
+
+    public RhinocerosBeetle() {
+        super(ID, info);
+        this.selfRetain=true;
+
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DrawCardAction(p, 1));
+    }
+
+    @Override
+    public void upgrade(){
+        if(!upgraded){
+            this.rarity = CardRarity.RARE;
+            upgradeName();
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new RhinocerosBeetle();
+    }
+
+}
