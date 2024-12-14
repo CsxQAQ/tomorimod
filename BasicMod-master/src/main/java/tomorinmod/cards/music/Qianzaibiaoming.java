e60f146a7bd743b73b5068b35bae4b107c3ec6cd
陈书玄 <1257150224@qq.com>
2024-12-14 19:26:11 +0800

    潜在表明

--- BasicMod-master/src/main/java/tomorinmod/cards/music/Qianzaibiaoming.java
+++ BasicMod-master/src/main/java/tomorinmod/cards/music/Qianzaibiaoming.java
@@ -2,12 +2,14 @@ package tomorinmod.cards.music;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import tomorinmod.character.MyCharacter;
+import tomorinmod.monitors.LunfuyuMonitor;
 import tomorinmod.powers.Gravity;
 import tomorinmod.util.CardStats;
 
@@ -22,30 +24,62 @@ public class Qianzaibiaoming extends BaseMusicCard {
     );
 
     public Qianzaibiaoming() {
-        super(ID, info);
-        this.musicUpgradeDamage=UPG_DAMAGE;
-        this.musicUpgradeMagic=UPG_MAGIC;
-        this.setDamage(DAMAGE,UPG_DAMAGE);
-        this.setMagic(MAGIC,UPG_MAGIC);
+        super(ID, info, new NumsInfo(
+                DAMAGE_COMMON, UPG_DAMAGE_COMMON, DAMAGE_UNCOMMON, UPG_DAMAGE_UNCOMMON, DAMAGE_RARE, UPG_DAMAGE_RARE,
+                BLOCK_COMMON, UPG_BLOCK_COMMON, BLOCK_UNCOMMON, UPG_BLOCK_UNCOMMON, BLOCK_RARE, UPG_BLOCK_RARE,
+                MAGIC_COMMON, UPG_MAGIC_COMMON, MAGIC_UNCOMMON, UPG_MAGIC_UNCOMMON, MAGIC_RARE, UPG_MAGIC_RARE
+        ));
     }
 
+    private final static int DAMAGE_COMMON = 5;
+    private final static int UPG_DAMAGE_COMMON = 3;
+    private final static int BLOCK_COMMON = 0;
+    private final static int UPG_BLOCK_COMMON = 0;
+    private final static int MAGIC_COMMON = 2;
+    private final static int UPG_MAGIC_COMMON = 1;
 
-    private final static int DAMAGE=6;
-    private final static int UPG_DAMAGE=3;
+    private final static int DAMAGE_UNCOMMON = 8;
+    private final static int UPG_DAMAGE_UNCOMMON = 4;
+    private final static int BLOCK_UNCOMMON = 0;
+    private final static int UPG_BLOCK_UNCOMMON = 0;
+    private final static int MAGIC_UNCOMMON = 3;
+    private final static int UPG_MAGIC_UNCOMMON = 1;
 
-    private final static int MAGIC=2;
-    private final static int UPG_MAGIC=1;
-
+    private final static int DAMAGE_RARE = 8;
+    private final static int UPG_DAMAGE_RARE = 4;
+    private final static int BLOCK_RARE = 0;
+    private final static int UPG_BLOCK_RARE = 0;
+    private final static int MAGIC_RARE = 3;
+    private final static int UPG_MAGIC_RARE = 1;
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+        if(musicRarity!=null){
+            if(musicRarity.equals(MusicRarity.RARE)) {
+                addToBot(new DamageAllEnemiesAction(p, damage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+            }else{
+                addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+            }
+        }
     }
 
     @Override
-    public void applyPowers(){
-        baseDamage=timesUpgraded*UPG_DAMAGE+DAMAGE+ AbstractDungeon.player.getPower(Gravity.POWER_ID).amount*magicNumber;
-        super.applyPowers();
+    public void update(){
+        super.update();
+        int gravityAmount=0;
+        if(AbstractDungeon.player.getPower(Gravity.POWER_ID)!=null){
+            gravityAmount=AbstractDungeon.player.getPower(Gravity.POWER_ID).amount;
+        }
+        if(musicRarity!=null){
+            if(musicRarity.equals(MusicRarity.RARE)){
+                this.target=CardTarget.ALL_ENEMY;
+                baseDamage=timesUpgraded*UPG_DAMAGE_RARE+DAMAGE_RARE+ gravityAmount*magicNumber;
+            }else if(musicRarity.equals(MusicRarity.UNCOMMON)){
+                baseDamage=timesUpgraded*UPG_DAMAGE_UNCOMMON+DAMAGE_UNCOMMON+ gravityAmount*magicNumber;
+            }else{
+                baseDamage=timesUpgraded*UPG_DAMAGE_COMMON+DAMAGE_COMMON+ gravityAmount*magicNumber;
+            }
+        }
     }
 
     @Override
