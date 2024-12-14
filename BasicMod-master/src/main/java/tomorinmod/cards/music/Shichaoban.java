b10390ad49a1682a4a55793c220f261b8f778cbe
陈书玄 <1257150224@qq.com>
2024-12-14 19:42:57 +0800

    诗超绊

--- BasicMod-master/src/main/java/tomorinmod/cards/music/Shichaoban.java
+++ BasicMod-master/src/main/java/tomorinmod/cards/music/Shichaoban.java
@@ -20,24 +20,45 @@ public class Shichaoban extends BaseMusicCard {
     );
 
     public Shichaoban() {
-        super(ID, info);
-        this.musicUpgradeDamage=UPG_DAMAGE;
-        this.musicUpgradeMagic=UPG_MAGIC;
-        this.setDamage(DAMAGE,UPG_DAMAGE);
-        this.setMagic(MAGIC,UPG_MAGIC);
+        super(ID, info, new NumsInfo(
+                DAMAGE_COMMON, UPG_DAMAGE_COMMON, DAMAGE_UNCOMMON, UPG_DAMAGE_UNCOMMON, DAMAGE_RARE, UPG_DAMAGE_RARE,
+                BLOCK_COMMON, UPG_BLOCK_COMMON, BLOCK_UNCOMMON, UPG_BLOCK_UNCOMMON, BLOCK_RARE, UPG_BLOCK_RARE,
+                MAGIC_COMMON, UPG_MAGIC_COMMON, MAGIC_UNCOMMON, UPG_MAGIC_UNCOMMON, MAGIC_RARE, UPG_MAGIC_RARE
+        ));
     }
 
 
-    private final static int DAMAGE=2;
-    private final static int UPG_DAMAGE=0;
+    private final static int DAMAGE_COMMON = 4;
+    private final static int UPG_DAMAGE_COMMON = 2;
+    private final static int BLOCK_COMMON = 0;
+    private final static int UPG_BLOCK_COMMON = 0;
+    private final static int MAGIC_COMMON = 6;
+    private final static int UPG_MAGIC_COMMON = 3;
 
-    private final static int MAGIC=2;
-    private final static int UPG_MAGIC=1;
+    private final static int DAMAGE_UNCOMMON = 6;
+    private final static int UPG_DAMAGE_UNCOMMON = 3;
+    private final static int BLOCK_UNCOMMON = 0;
+    private final static int UPG_BLOCK_UNCOMMON = 0;
+    private final static int MAGIC_UNCOMMON = 9;
+    private final static int UPG_MAGIC_UNCOMMON = 5;
+
+    private final static int DAMAGE_RARE = 6;
+    private final static int UPG_DAMAGE_RARE = 3;
+    private final static int BLOCK_RARE = 0;
+    private final static int UPG_BLOCK_RARE = 0;
+    private final static int MAGIC_RARE = 9;
+    private final static int UPG_MAGIC_RARE = 5;
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
-        baseDamage=baseDamage*magicNumber;
+        if(musicRarity!=null){
+            if(musicRarity.equals(MusicRarity.RARE)){
+                baseDamage=baseDamage*2;
+            }else{
+                baseDamage=baseDamage+magicNumber;
+            }
+        }
     }
 
     @Override
