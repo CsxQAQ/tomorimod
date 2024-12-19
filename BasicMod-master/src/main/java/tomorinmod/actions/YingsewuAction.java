9e6aad0f7e42ce8e9081fe750652c87dd0185284
陈书玄 <1257150224@qq.com>
2024-12-19 23:58:38 +0800

    适配影色舞

--- BasicMod-master/src/main/java/tomorinmod/actions/YingsewuAction.java
+++ BasicMod-master/src/main/java/tomorinmod/actions/YingsewuAction.java
@@ -1,8 +1,15 @@
 package tomorinmod.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import tomorinmod.cards.forms.forms.BaseFormCard;
+import tomorinmod.cards.music.FailComposition;
 import tomorinmod.monitors.HandleFormsMonitor;
+import tomorinmod.powers.forms.BaseFormPower;
+import tomorinmod.powers.forms.FormEffect;
+
+import static tomorinmod.BasicMod.makeID;
 
 public class YingsewuAction extends AbstractGameAction {
 
@@ -11,7 +18,13 @@ public class YingsewuAction extends AbstractGameAction {
     }
 
     public void update() {
-        //HandleFormsMonitor.applyCurrentFormEffect(BaseFormCard.curForm);
+        for(AbstractPower power: AbstractDungeon.player.powers){
+            if(power.ID.equals(makeID(BaseFormCard.curForm))){
+                if(power instanceof FormEffect){
+                    ((FormEffect)power).applyEffectPower();
+                }
+            }
+        }
         this.isDone = true;
     }
 
