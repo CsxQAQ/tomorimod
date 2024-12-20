a338e9e292213b12ed9feb904e0366d7244ed073
陈书玄 <1257150224@qq.com>
2024-12-21 00:38:54 +0800

    反转还是光与影好

--- BasicMod-master/src/main/java/tomorinmod/cards/customcards/Reversal.java
+++ BasicMod-master/src/main/java/tomorinmod/cards/customcards/Reversal.java
@@ -8,6 +8,11 @@ import tomorinmod.cards.BaseCard;
 import tomorinmod.character.MyCharacter;
 import tomorinmod.util.CardStats;
 
+import java.util.ArrayList;
+
+import static com.badlogic.gdx.Gdx.graphics;
+import static tomorinmod.BasicMod.imagePath;
+
 public class Reversal extends BaseCard {
     public static final String ID = makeID(Reversal.class.getSimpleName());
     private static final CardStats info = new CardStats(
@@ -18,6 +23,36 @@ public class Reversal extends BaseCard {
             1
     );
 
+    private static final ArrayList<String> imagePaths = new ArrayList<>();
+    private float timeElapsed = 0f;
+    private float interval = 0.05f;
+    private int currentImageIndex = 0;
+    private static final int totalImages = 43;
+
+
+    static {
+        for (int i = 0; i <= totalImages; i++) {
+
+            imagePaths.add(imagePath("cards/Reversal/" + i + ".png"));
+        }
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        timeElapsed += graphics.getDeltaTime();
+
+        if (timeElapsed >= interval) {
+            timeElapsed = 0f;
+            currentImageIndex++;
+
+            if (currentImageIndex >= totalImages) {
+                currentImageIndex = 0;
+            }
+
+            loadCardImage(imagePaths.get(currentImageIndex));
+        }
+    }
     public Reversal() {
         super(ID, info);
     }
