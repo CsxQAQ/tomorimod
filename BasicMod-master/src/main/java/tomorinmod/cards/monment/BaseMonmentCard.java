9da025b2d20c651c46cd0b3b8243281e5a67ec4d
陈书玄 <1257150224@qq.com>
2024-12-21 16:52:22 +0800

    瞬间牌的masterdeck消耗特效

--- BasicMod-master/src/main/java/tomorinmod/cards/monment/BaseMonmentCard.java
+++ BasicMod-master/src/main/java/tomorinmod/cards/monment/BaseMonmentCard.java
@@ -1,12 +1,18 @@
 package tomorinmod.cards.monment;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.ExhaustEmberEffect;
+import com.megacrit.cardcrawl.vfx.cardManip.ExhaustCardEffect;
 import tomorinmod.cards.BaseCard;
 import tomorinmod.character.MyCharacter;
 import tomorinmod.util.CardStats;
+import tomorinmod.vfx.ExhaustMasterDeckEffect;
 
 import java.util.Iterator;
 
@@ -30,8 +36,14 @@ public abstract class BaseMonmentCard extends BaseCard {
             AbstractCard card = iterator.next();
             if (aCard.uuid.equals(card.uuid)) {
                 iterator.remove();
+                addExhaustEffectOnMasterDeck();
                 break;
             }
         }
+    }
+
+    public static void addExhaustEffectOnMasterDeck(){
+        AbstractDungeon.effectsQueue.add(new ExhaustMasterDeckEffect(
+                Settings.WIDTH -100.0f*Settings.scale, Settings.HEIGHT -100.0f*Settings.scale));
     }
 }
