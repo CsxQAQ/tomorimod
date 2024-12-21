052932626687ae63eaf849d63105f1cd1f5475b9
陈书玄 <1257150224@qq.com>
2024-12-21 19:23:15 +0800

    命运共同体

--- BasicMod-master/src/main/java/tomorinmod/cards/monment/FateCommunity.java
+++ BasicMod-master/src/main/java/tomorinmod/cards/monment/FateCommunity.java
@@ -3,14 +3,17 @@ package tomorinmod.cards.monment;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import tomorinmod.actions.MultiSmoothHealthChangeAction;
 import tomorinmod.actions.SmoothHealthChangeAction;
 import tomorinmod.character.MyCharacter;
 import tomorinmod.util.CardStats;
 import tomorinmod.vfx.FateCommunityEffect;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FateCommunity extends BaseMonmentCard {
     public static final String ID = makeID(FateCommunity.class.getSimpleName());
@@ -19,7 +22,7 @@ public class FateCommunity extends BaseMonmentCard {
             CardType.SKILL,
             CardRarity.RARE,
             CardTarget.ALL,
-            3
+            1
     );
 
     public FateCommunity() {
@@ -50,19 +53,36 @@ public class FateCommunity extends BaseMonmentCard {
                 int totalUnits = activeMonsterCount + 1;
                 double averagePercentage = totalPercentage / totalUnits;
 
+                // 准备好所有目标
+                List<AbstractCreature> allTargets = new ArrayList<>();
+                List<Integer> startHealths = new ArrayList<>();
+                List<Integer> endHealths   = new ArrayList<>();
+
+                // 先加玩家
+                allTargets.add(p);
+                startHealths.add(p.currentHealth);
                 int newPlayerHealth = (int) Math.ceil(p.maxHealth * averagePercentage);
-                addToBot(new SmoothHealthChangeAction(p, p.currentHealth, Math.max(1, newPlayerHealth)));
+                endHealths.add(Math.max(1, newPlayerHealth));
 
-                AbstractDungeon.effectsQueue.add(new FateCommunityEffect(p));
-
+                // 再加存活怪物
                 for (AbstractMonster monster : monsters) {
                     if (!monster.isDeadOrEscaped()) {
+                        allTargets.add(monster);
+                        startHealths.add(monster.currentHealth);
                         int newMonsterHealth = (int) Math.ceil(monster.maxHealth * averagePercentage);
-                        addToBot(new SmoothHealthChangeAction(monster, monster.currentHealth, Math.max(1, newMonsterHealth)));
-                        AbstractDungeon.effectsQueue.add(new FateCommunityEffect(monster));
+                        endHealths.add(Math.max(1, newMonsterHealth));
                     }
                 }
-                isDone=true;
+
+                // 调用 多单位平滑改血 Action
+                addToBot(new MultiSmoothHealthChangeAction(allTargets, startHealths, endHealths));
+
+                // 你要显示的效果也可以一次性加入
+                for (AbstractCreature c : allTargets) {
+                    AbstractDungeon.effectsQueue.add(new FateCommunityEffect(c));
+                }
+
+                isDone = true;
             }
         });
     }
@@ -71,7 +91,7 @@ public class FateCommunity extends BaseMonmentCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeBaseCost(2);
+            upgradeBaseCost(0);
         }
     }
 
