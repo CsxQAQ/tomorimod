397000a1d970eea1af798ea328d499764eefd9cb
陈书玄 <1257150224@qq.com>
2024-12-22 11:26:35 +0800

    加个音效

--- BasicMod-master/src/main/java/tomorinmod/powers/custompowers/TomorinApotheosisPower.java
+++ BasicMod-master/src/main/java/tomorinmod/powers/custompowers/TomorinApotheosisPower.java
@@ -1,11 +1,13 @@
 package tomorinmod.powers.custompowers;
 
+import basemod.BaseMod;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.unique.ApotheosisAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import tomorinmod.cards.music.BaseMusicCard;
 import tomorinmod.powers.BasePower;
@@ -21,19 +23,27 @@ public class TomorinApotheosisPower extends BasePower {
         super(POWER_ID, TYPE, TURN_BASED, owner, 0);
     }
 
+    private boolean isAnyCardUpgrade=false;
+
     @Override
     public void atStartOfTurn() {
+        isAnyCardUpgrade=false;
         AbstractPlayer p= AbstractDungeon.player;
         upgradeAllCardsInGroup(p.hand);
         upgradeAllCardsInGroup(p.drawPile);
         upgradeAllCardsInGroup(p.discardPile);
         upgradeAllCardsInGroup(p.exhaustPile);
+        if(isAnyCardUpgrade){
+            this.flash();
+            CardCrawlGame.sound.play("CARD_UPGRADE");
+        }
     }
 
     private void upgradeAllCardsInGroup(CardGroup cardGroup) {
         for (AbstractCard c : cardGroup.group) {
             if(c instanceof BaseMusicCard){
                 c.upgrade();
+                isAnyCardUpgrade=true;
             }else{
                 if (c.canUpgrade()) {
                     if (cardGroup.type == CardGroup.CardGroupType.HAND) {
@@ -41,6 +51,7 @@ public class TomorinApotheosisPower extends BasePower {
                     }
                     c.upgrade();
                     c.applyPowers();
+                    isAnyCardUpgrade=true;
                 }
             }
         }
