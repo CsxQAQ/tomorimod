7f016a5fb8c66ad84ac35414e9e6fcb16c9c76d8
陈书玄 <1257150224@qq.com>
2025-01-08 22:18:59 +0800

    灵视

---
+++ BasicMod-master/src/main/java/tomorimod/cards/customcards/SoulView.java
@@ -0,0 +1,59 @@
+package tomorimod.cards.customcards;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import tomorimod.actions.cardactions.SoulViewAction;
+import tomorimod.cards.BaseCard;
+import tomorimod.character.Tomori;
+import tomorimod.patches.AbstractCardSetMaterialPatch;
+import tomorimod.util.CardStats;
+
+public class SoulView extends BaseCard {
+    public static final String ID = makeID(SoulView.class.getSimpleName());
+    public static final CardStats info = new CardStats(
+            Tomori.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF,
+            1
+    );
+
+    public SoulView() {
+        super(ID, info);
+        exhaust=true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new SoulViewAction(1));
+    }
+
+    public void updateDescription() {
+
+        if(upgraded){
+            this.rawDescription = CardCrawlGame.languagePack.getCardStrings(ID).EXTENDED_DESCRIPTION[0];
+        }else{
+            this.rawDescription = CardCrawlGame.languagePack.getCardStrings(ID).DESCRIPTION;
+        }
+        initializeDescription();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            exhaust=false;
+            this.updateDescription();
+        }
+    }
+
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new SoulView();
+    }
+
+}
