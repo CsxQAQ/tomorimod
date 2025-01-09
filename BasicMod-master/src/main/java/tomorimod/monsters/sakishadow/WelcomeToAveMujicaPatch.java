f7ea38315debf4d41726f2ec1e5566c5d1c64214
陈书玄 <1257150224@qq.com>
2025-01-09 23:40:21 +0800

    优化

--- BasicMod-master/src/main/java/tomorimod/monsters/sakishadow/WelcomeToAveMujicaPatch.java
+++ BasicMod-master/src/main/java/tomorimod/monsters/sakishadow/WelcomeToAveMujicaPatch.java
@@ -3,6 +3,7 @@ package tomorimod.monsters.sakishadow;
 import com.badlogic.gdx.math.Vector2;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.EmptyDeckShuffleAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -27,27 +28,63 @@ public class WelcomeToAveMujicaPatch {
             method="preBattlePrep"
     )
     public static class AbstractPlayerPreBattlePrepPatch{
-        @SpireInsertPatch(
-                locator = Locator.class
-        )
-        public static void insert(AbstractPlayer __instance){
+        @SpirePostfixPatch
+        public static void postfix(AbstractPlayer __instance){
             for(AbstractMonster monster:AbstractDungeon.getCurrRoom().monsters.monsters){
                 if(monster instanceof SakiShadowMonster){
                     __instance.drawPile.clear();
+                }
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz= AbstractPlayer.class,
+            method="applyStartOfCombatPreDrawLogic"
+    )
+    public static class AbstractPlayerStartOfCombatPreDrawPatch{
+        @SpirePostfixPatch
+        public static void postfix(AbstractPlayer __instance){
+            for(AbstractMonster monster:AbstractDungeon.getCurrRoom().monsters.monsters){
+                if(monster instanceof SakiShadowMonster){
+                    AbstractDungeon.actionManager.addToBottom(new AbstractGameAction() {
+                        @Override
+                        public void update() {
+                            __instance.hand.clear();
+                            isDone=true;
+                        }
+                    });
                     AbstractDungeon.actionManager.addToBottom(new MakeTempCardInHandAction(new WelcomeToAveMujica()));
                 }
             }
         }
-
-        private static class Locator extends SpireInsertLocator {
-            public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
-                Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractPlayer.class, "drawPile");
-
-                int[] lines = LineFinder.findInOrder(ctMethodToPatch, new ArrayList<Matcher>(), finalMatcher);
-                return new int[]{lines[0]+1};
-            }
-        }
     }
+//    @SpirePatch(
+//            clz= AbstractPlayer.class,
+//            method="preBattlePrep"
+//    )
+//    public static class AbstractPlayerPreBattlePrepPatch{
+//        @SpireInsertPatch(
+//                locator = Locator.class
+//        )
+//        public static void insert(AbstractPlayer __instance){
+//            for(AbstractMonster monster:AbstractDungeon.getCurrRoom().monsters.monsters){
+//                if(monster instanceof SakiShadowMonster){
+//                    __instance.drawPile.clear();
+//                    AbstractDungeon.actionManager.addToBottom(new MakeTempCardInHandAction(new WelcomeToAveMujica()));
+//                }
+//            }
+//        }
+//
+//        private static class Locator extends SpireInsertLocator {
+//            public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+//                Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractPlayer.class, "drawPile");
+//
+//                int[] lines = LineFinder.findInOrder(ctMethodToPatch, new ArrayList<Matcher>(), finalMatcher);
+//                return new int[]{lines[0]+1};
+//            }
+//        }
+//    }
 
 
 
